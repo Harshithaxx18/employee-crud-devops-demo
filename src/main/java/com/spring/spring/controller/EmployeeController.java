8e5756a78523dcd95d7f373a42e14eb17fb7b170
Harshitha Vattipalli <vattipalliharshitha18@gmail.com>
2026-07-27 22:50:10 +0530

    Update employee API

--- src/main/java/com/spring/spring/controller/EmployeeController.java
+++ src/main/java/com/spring/spring/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+//Employee CRUD API
 @RestController
 public class EmployeeController {
     private final EmployeeService service;
